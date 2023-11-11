2f45c29c7ebc8987f0558a5f3780560506b72601
Elijah Love <=>
2023-11-10 20:03:35 -0500

    Fixed boot device section

--- system/nixos/hosts/legoshi/hardware.nix
+++ system/nixos/hosts/legoshi/hardware.nix
@@ -18,16 +18,56 @@
     };
   };
 
-#  fileSystems = {
-#    "/" = {
-#      device = "/dev/disk/by-label/nixos";
-#      fsType = "ext4";
-#    };
-#    "/boot" = {
-#      device = "/dev/disk/by-label/boot";
-#      fsType = "vfat";
-#    };
-#  };
+    fileSystems."/" =
+    { device = "/dev/disk/by-uuid/7a60625a-f54b-4ba2-bb0c-783465f7c65f";
+      fsType = "btrfs";
+      options = [ "subvol=root" ];
+    };
+
+  boot.initrd.luks.devices."terra-crypt".device = "/dev/disk/by-uuid/abeb2489-cbe6-4be7-8dae-32e573dd479b";
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/7a60625a-f54b-4ba2-bb0c-783465f7c65f";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/7a60625a-f54b-4ba2-bb0c-783465f7c65f";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  fileSystems."/persist" =
+    { device = "/dev/disk/by-uuid/7a60625a-f54b-4ba2-bb0c-783465f7c65f";
+      fsType = "btrfs";
+      options = [ "subvol=persist" ];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/disk/by-uuid/7a60625a-f54b-4ba2-bb0c-783465f7c65f";
+      fsType = "btrfs";
+      options = [ "subvol=log" ];
+    };
+
+  fileSystems."/swap" =
+    { device = "/dev/disk/by-uuid/7a60625a-f54b-4ba2-bb0c-783465f7c65f";
+      fsType = "btrfs";
+      options = [ "subvol=swap" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/1930-669A";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ ];
+
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
 
 #  swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
 
