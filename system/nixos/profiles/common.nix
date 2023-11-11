ec1938c11599038da18a2150c9b52c19a737046f
Elijah Love <=>
2023-11-11 14:46:13 -0500

    Updated user

--- system/nixos/profiles/common.nix
+++ system/nixos/profiles/common.nix
@@ -54,7 +54,7 @@ in
       curl
       git
       pciutils
-      vim
+      neovim
     ];
   };
 }
---
+++ user/elijahdl.nix
@@ -0,0 +1,6 @@
+{
+  name = "elijahdl";
+  email = "me@elijahlove.xyz";
+  #signingKey = "5A038CEFD458DB47A6135B3F8316DECECB1A3F10";
+  hashedPassword = "$6$gdUPSRFP8Jrykmd.$FsOhN5nHoPnbqlHGwf3Qxz.3lMk1agN5HgP60wOSN8H4GAoEdnZCztSdZuMRoDbo8qYmotmz44Ma.cfUuqkMW/";
+}
