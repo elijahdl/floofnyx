ce6b5653026fa93c0c0577dc16b57729c8582f5a
Elijah Love <=>
2023-11-11 19:55:48 -0500

    Fixed convenience script for testing

--- 00_prepare-setup-for-qemu.sh
+++ 00_prepare-setup-for-qemu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i '' 's/nvme0n1p1/vda1/g' system/nixos/hosts/legoshi/setup.sh
-sed -i '' 's/nvme0n1p2/vda2/g' system/nixos/hosts/legoshi/setup.sh
-sed -i '' 's/nvme0n1/vda/g' system/nixos/hosts/legoshi/setup.sh
+sed -i '' -e 's/nvme0n1p1/vda1/g' system/nixos/hosts/legoshi/setup.sh
+sed -i '' -e 's/nvme0n1p2/vda2/g' system/nixos/hosts/legoshi/setup.sh
+sed -i '' -e 's/nvme0n1/vda/g' system/nixos/hosts/legoshi/setup.sh
